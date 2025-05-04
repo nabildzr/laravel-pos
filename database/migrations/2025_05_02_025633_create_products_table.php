8b2c4aca961ca42ac3c9a6481eb3fec1e75c6cfc
nabildzr <muhammadnbillz@gmail.com>
2025-05-04 11:13:41 +0700

    first commit

---
+++ database/migrations/2025_05_02_025633_create_products_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 255);
+            $table->decimal('price', 12, 2);
+            $table->decimal('cost', 12, 2);
+            $table->string('sku')->unique();
+            $table->integer('stock')->default(0);
+            $table->string('unit');
+            $table->text('description')->nullable();
+            $table->boolean('is_active')->default(false);
+            $table->foreignId('user_id')->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
