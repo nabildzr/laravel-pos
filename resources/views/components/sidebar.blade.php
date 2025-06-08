d116e8a76b41ced5a86eb64b70e3c7d0f6fcc50c
nabildzr <muhammadnbillz@gmail.com>
2025-06-08 14:56:20 +0700

    fix: Correct user role condition in sidebar for admin and super_admin checks

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -90,7 +90,7 @@
             </li>
 
 
-            @if (Auth::user()->role == ('admin' && 'super_admin'))
+            @if (Auth::user()->role === 'admin' || Auth::user()->role === 'super_admin')
                 <li>
                     <a href="{{ url('/payment-method') }}">
                         <iconify-icon icon="fluent:payment-48-filled" class="menu-icon"></iconify-icon>
